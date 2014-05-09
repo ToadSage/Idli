323023de74f6ced8b0b3930ef5e76ac73049515f
Deekshay <deeksha.blr@gmail.com>
2014-05-09 18:51:35 -0400

    Update rfid_detector.h

--- idli/Include/rfid_detector.h
+++ idli/Include/rfid_detector.h
@@ -11,7 +11,7 @@ class rfid_detector
 libusb_device **device_list; //pointer to pointer of device, used to retrieve a list of devices
 libusb_device_handle *device_handle; //a device handle
 libusb_context *USB_session = NULL; //a libusb session
-unsigned char *data = new unsigned char[10];	 //data to be read, and the maximum size of the buffer into which the data is read
+unsigned char *data = new unsigned char[10];	 //data to be read, and the size of the buffer into which the data is read
 
 string rfid();
 
